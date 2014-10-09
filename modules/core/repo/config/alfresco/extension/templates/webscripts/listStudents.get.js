4c22b9612004ce78a272767667c71c11d159e169
Kirill <kirill@localhost.localdomain>
2014-10-09 19:54:16 +0400

    Some enhancements to js

--- modules/core/repo/config/alfresco/extension/templates/webscripts/listStudents.get.js
+++ modules/core/repo/config/alfresco/extension/templates/webscripts/listStudents.get.js
@@ -4,8 +4,6 @@ var dlc = st.getContainer('dataLists');
 var xpath = './*[@dl:dataListItemType="hse:studentList"]';
 var dl = dlc.childrenByXPath(xpath)[0];
 
-
-
 model.students = [];
 
 for each(var dli in dl.children)
@@ -15,14 +13,14 @@ for each(var dli in dl.children)
 	});
 
 model.students.sort(function(a, b){
-	if (a.group>b.group)
+	if (a.group > b.group)
 		return 1;
-	else if (a.group<b.group)
+	else if (a.group < b.group)
 		return -1;
 	else
-		if (a.name>b.name)
+		if (a.name > b.name)
 			return 1;
-		else if (a.name<b.name)
+		else if (a.name < b.name)
 			return -1;
 		else
 			return 0;
